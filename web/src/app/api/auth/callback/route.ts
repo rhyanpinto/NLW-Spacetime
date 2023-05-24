c908789ef0e083cbf17e04262099657228bcd69f
Rhyan Pinto <rhyanpinto@gmail.com>
2023-05-24 12:32:32 -0300

    Aula 04

--- web/src/app/api/auth/callback/route.ts
+++ web/src/app/api/auth/callback/route.ts
@@ -5,19 +5,21 @@ export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url)
   const code = searchParams.get('code')
 
+  const redirectTo = request.cookies.get('redirectTo')?.value
+
   const registerResponse = await api.post('/register', {
     code,
   })
 
   const { token } = registerResponse.data
 
-  const redirectURL = new URL('/', request.url)
+  const redirectURL = redirectTo ?? new URL('/', request.url)
 
   const cookieExpiresInSeconds = 60 * 60 * 24 * 30 // 30 days
 
   return NextResponse.redirect(redirectURL, {
     headers: {
-      'Set-Cookie': `token=${token}; Path=/; max-age=${cookieExpiresInSeconds}; HttpOnly; Secure; SameSite=Strict`,
+      'Set-Cookie': `token=${token}; Path=/; max-age=${cookieExpiresInSeconds}; Secure; SameSite=Strict`,
     },
   })
 }
